29641782d5d511a8d28efb77bcccbd61e9c33ea4
Clara Mendes <claraplanejamento@gmail.com>
2022-10-17 16:47:11 -0300

    :recycle: adds rendering to action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,7 @@ class RoomsController < ApplicationController
     user = User.find_or_create_by(name: params[:user])
     room = Room.find_or_create_by(name: params[:room])
     room.add_user(user)
+
+    render json: room
   end
 end
