f2efb327437d1df27af1ab9ab9faec5aaf78645e
Clara Mendes <claraplanejamento@gmail.com>
2022-10-17 16:57:08 -0300

    :sparkles: adds message index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   post 'rooms/join' => 'rooms#join'
+  get 'rooms/:room_id/messages' => 'messages#index'
   # resources :scaffold_tests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
