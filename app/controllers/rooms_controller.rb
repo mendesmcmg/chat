de630daad552268a1298af05127b631a5342400f
Clara Mendes <claraplanejamento@gmail.com>
2022-10-17 19:00:13 -0300

    :sparkles: adds leave room action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,4 +6,12 @@ class RoomsController < ApplicationController
 
     render json: room
   end
+
+  def leave
+    current_user_room = UserRoom.find_by(:user_id => params[:user_id], :room_id => params[:room_id])
+    current_user_room.active = false
+    current_user_room.save
+
+    render json: current_user_room
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post 'rooms/join' => 'rooms#join'
+  post 'rooms/:room_id/leave' => 'rooms#leave'
+
   get 'rooms/:room_id/messages' => 'messages#index'
   post 'rooms/:room_id/messages' => 'messages#create'
   # resources :scaffold_tests
