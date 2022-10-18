7c5b042eba85ecb8cd1f55776dd45d80f2cce44e
Clara Mendes <claraplanejamento@gmail.com>
2022-10-18 16:35:36 -0300

    :technologist: makes pagination optional

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    messages = Message.where(:room_id => params[:room_id] ).order(:created_at).page params[:page]
+    messages = Message.where(:room_id => params[:room_id] ).order(:created_at).page(params[:page] || 1)
 
     render json: messages
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'rooms/:room_id/leave' => 'rooms#leave'
   get 'rooms/:room_id/active_users' => 'rooms#active_users'
 
-  get 'rooms/:room_id/messages/page/:page' => 'messages#index'
+  get 'rooms/:room_id/messages(/page/:page)' => 'messages#index'
   post 'rooms/:room_id/messages' => 'messages#create'
   # resources :scaffold_tests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
