317c82dab3b548403799e7e60696f091430a13cd
Clara Mendes <claraplanejamento@gmail.com>
2022-10-20 06:38:12 -0300

    :art: adds rubocop

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Messages", type: :request do
-  describe "GET rooms/:room_id/messages(/page/:page)" do
+RSpec.describe 'Messages', type: :request do
+  describe 'GET rooms/:room_id/messages(/page/:page)' do
     it 'gets paginated messages' do
       room1 = Room.create(name: 'no1')
       user1 = User.create(name: 'no1')
-      for i in 0..20 do
+      21.times do |i|
         Message.create(user_id: user1.id, room_id: room1.id, text: "oi #{i}")
       end
 
       get "/rooms/#{room1.id}/messages"
       body = JSON.parse(response.body)
-      
+
       expect(Message.all.length).to eq 21
       expect(body.length).to eq 10
     end
@@ -25,7 +27,7 @@ RSpec.describe "Messages", type: :request do
       post "/rooms/#{room1.id}/messages?user_id=#{user1.id}&text=mensagem-enviada"
       body = JSON.parse(response.body)
 
-      expect(body["text"]).to eq "mensagem-enviada"
+      expect(body['text']).to eq 'mensagem-enviada'
     end
   end
 end
