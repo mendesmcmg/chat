443d12c5c11c70a31b79b0bd72833045566451b6
Clara Mendes <claraplanejamento@gmail.com>
2022-10-20 06:05:50 -0300

    :white_check_mark: adds rooms controller tests

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -4,8 +4,29 @@ require 'rails_helper'
 require 'json'
 
 RSpec.describe 'Rooms', type: :request do
-  # post 'rooms/join' => 'rooms#join'
   # post 'rooms/:room_id/leave' => 'rooms#leave'
+
+  describe 'POST /rooms/join' do
+    it 'adds user to room' do
+      post "/rooms/join?user=1user&room=1room"
+
+      expect(User.last.name).to eq ("1user")
+      expect(Room.last.name).to eq ("1room")
+      expect(UserRoom.last.active).to eq (true)
+    end
+  end
+
+  describe 'POST /rooms/:room_id/leave' do
+    it 'makes user leave room' do
+      user1 = User.create!(name: 'no1')
+      room1 = Room.create!(name: 'no1')
+      room1.add_user(user1)
+
+      post "/rooms/#{room1.id}/leave?user_id=#{user1.id}"
+
+      expect(UserRoom.last.active).to eq false
+    end
+  end
 
   describe 'GET /:room_id/active_users' do
     it 'retrieves active users with one user' do
