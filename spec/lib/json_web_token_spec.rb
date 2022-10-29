55618a868248efa2f00d17e3a73476c54cd6e0e5
Clara Mendes <claraplanejamento@gmail.com>
2022-10-29 11:50:11 -0300

    :white_check_mark: adds jwt encode/decode tests

---
+++ spec/lib/json_web_token_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'json_web_token'
+
+RSpec.describe 'json_web_token' do
+  it 'retrieves correct id after decoding' do
+    user = User.create(name: 'TestUser')
+
+    token = JsonWebToken.encode({ id: user.id })
+    decoded = JsonWebToken.decode(token)
+    expect(decoded[:id]).to eq user.id
+  end
+
+  it 'sets default expiration time' do
+    user = User.create(name: 'TestUser')
+
+    token = JsonWebToken.encode({ id: user.id })
+    decoded = JsonWebToken.decode(token)
+    expect(decoded[:exp]).to be_truthy
+  end
+end
